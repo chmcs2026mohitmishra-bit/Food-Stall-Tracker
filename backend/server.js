1f17fb5b80bf67d012480352c8e6045deb46215a
chmcs2026mohitmishra-bit <chmcs2026.mohit.mishra@gmail.com>
2026-03-03 16:15:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,37 +2,30 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import foodRoutes from "./routes/foodRoutes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-
-// Better CORS Config
-
-// Database Connection
-mongoose.connect("mongodb://127.0.0.1:27017/foodstall")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch(err => console.log("DB Connection Error:", err));
 
-// Routes
 app.use("/food", foodRoutes);
 
-// Debug Route - to check all routes working
 app.get("/food/test", (req, res) => {
   res.json({ message: "Food API is working" });
 });
 
-// Root Test Route
 app.get("/", (req, res) => {
   res.send("Food Stall Tracker Backend Running");
 });
 
-// 404 Handler
 app.use((req, res) => {
   res.status(404).json({
     message: "Route Not Found",
@@ -43,5 +36,5 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

f38fdd35bf481ebc0362eb6c49c18c36431d2c8d
chmcs2026mohitmishra-bit <chmcs2026.mohit.mishra@gmail.com>
2026-03-03 16:30:07 +0530

    Update API base URL to production endpoint

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/food",
+  baseURL: "https://food-stall-tracker.onrender.com/food",
 });
 
 export default api;
+
